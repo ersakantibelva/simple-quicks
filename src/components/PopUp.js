be0c6a1d63214e5f2f77187f287e06662a0d524a
ersakantibelva <ersakantibelva@gmail.com>
2023-01-17 19:15:47 +0700

    feat/task components

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,11 +1,13 @@
 import Chatroom from "./Chatroom";
 import Inbox from "./Inbox";
+import Task from "./Task";
 
 export default function PopUp() {
   return (
     <div className="w-[734px] h-[737px] bg-white absolute rounded-lg bottom-[110px] right-[24px]">
       {/* <Inbox /> */}
-      <Chatroom />
+      {/* <Chatroom /> */}
+      <Task />
     </div>
   );
 }
